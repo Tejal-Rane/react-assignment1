e8b3cfb0b143599d486c177cb39e34b54fee5283
Tejal-Rane <87368902+Tejal-Rane@users.noreply.github.com>
2021-07-13 17:02:16 +0530

    Day 1 assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./style.css"
+import Test from "./Test" 
+import Button from "./button"
+
+ReactDOM.render(<Test />, document.getElementById('root'));
+
+
